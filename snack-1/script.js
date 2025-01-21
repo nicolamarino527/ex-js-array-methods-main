b8f59d0fb27a7829e1f16929f042752545ec32df
nicolamarino <nicolamarino527@gmail.com>
2025-01-21 19:36:27 +0100

    commit snack-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,12 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
+
+// names.forEach(showNames);
+// function showNames(name) {
+//     console.log(name);
+// }
+
+names.forEach (name => console.log(name));
+
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
