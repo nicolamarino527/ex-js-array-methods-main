73b45b21197cc641387959df76dd465b6c1eacac
nicolamarino <nicolamarino527@gmail.com>
2025-01-21 20:36:29 +0100

    commit snack-7

--- snack-7/script.js
+++ snack-7/script.js
@@ -19,5 +19,19 @@ const students = [
   },
 ];
 
+
+
 // Recupera i dati dello studente con id 2
+
+
+// for (i = 0; i = students.length; i++) {
+//   let student = student[1];
+// }
+
+let idNumber = 2;
+
+let student = students.filter(number => number.id === idNumber);
+console.log(student);
+
+
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
