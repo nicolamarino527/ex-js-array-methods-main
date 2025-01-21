da2a8c4f30d34ef8c4c05b6b7355d26097d21600
nicolamarino <nicolamarino527@gmail.com>
2025-01-21 20:14:52 +0100

    commit snack-5

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,8 @@
 const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
+const evenNums = nums.filter(even => even % 2 === 0 );getComputedStyle
+console.log(evenNums);
+
+
 // Risultato: [2, 8, 4, 12]
